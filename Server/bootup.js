a1ec331877bf714764526311b7e6adf3dc06f223
Graham Reeves <graham@grahamreeves.com>
2020-05-31 20:31:19 +0100

    Generic robust player-ordering

--- Server/bootup.js
+++ Server/bootup.js
@@ -18,6 +18,14 @@ class TGame
 	constructor()
 	{
 		this.Players = [];
+		
+		//	keep a record of last-playing players
+		//	then when we need the next player, we pick someone not in the list
+		//	if all exhausted, we know the player who hasn't had a go for the longest
+		this.LastPlayers = [];
+		//	we still need to keep the current one as we get next player, but doesnt
+		//	move along until end of turn
+		this.NextPlayer = null;
 	}
 	
 	AddPlayer(PlayerRef)
@@ -32,6 +40,56 @@ class TGame
 		this.Players = this.Players.filter( p => p!=PlayerRef );
 	}
 	
+	//	this gets the next player, but does NOT move it along
+	//	in case move is not completed. EndPlayerTurn() moves it along
+	GetNextPlayer()
+	{
+		if ( !this.Players.length )
+			throw `GetNextPlayer: No players to choose from`;
+
+		//	player still pending
+		if ( this.NextPlayer !== null )
+			return this.NextPlayer;
+		
+		//	cull any players that have left from the played list
+		function IsPlayer(Player)
+		{
+			return this.Players.some( p => p==Player );
+		}
+		this.LastPlayers = this.LastPlayers.filter(IsPlayer.bind(this));
+		
+		//	go through players to see if one hasn't played
+		for ( let i=0;	i<this.Players.length;	i++ )
+		{
+			const Player = this.Players[i];
+			//	already played
+			if ( this.LastPlayers.some( p => p==Player ) )
+				continue;
+			//	hasn't played, set active
+			this.NextPlayer = Player;
+			return Player;
+		}
+		
+		//	everyone has played, take the oldest one and return it
+		const FiloPlayer = this.LastPlayers.shift();
+		this.NextPlayer = FiloPlayer;
+		return FiloPlayer;
+	}
+	
+	EndPlayerTurn(ExpectedPlayer=null)
+	{
+		if ( this.NextPlayer === null )
+			throw `EndPlayerTurn(${ExpectedPlayer}) expecting no null-nextplayer`;
+		
+		//	make sure we're ending the right player
+		if ( this.NextPlayer != ExpectedPlayer )
+			throw `EndPlayerTurn(${ExpectedPlayer}) should be ${this.NextPlayer}`;
+		
+		//	put this player at the end of the list
+		this.LastPlayers.push(this.NextPlayer);
+		this.NextPlayer = null;
+	}
+	
 	async WaitForNextMove()			{	throw `Game has not overloaded WaitForNextMove`;	}
 	async InitNewPlayer(PlayerRef)	{	throw `Game has not overloaded InitNewPlayer`;	}
 }
@@ -42,7 +100,6 @@ class TPickANumberGame extends TGame
 	constructor()
 	{
 		super(...arguments);
-		this.CurrentPlayerIndex = 0;
 		
 		this.State = this.InitState();
 	}
@@ -76,9 +133,9 @@ class TPickANumberGame extends TGame
 	
 	async GetNextMove()
 	{
-		//	todo: handle players being removed, this.CurrentPlayerIndex should be last player
+		const NextPlayer = this.GetNextPlayer();
 		const Move = {};
-		Move.Player = this.Players[this.CurrentPlayerIndex%this.Players.length];
+		Move.Player = NextPlayer;
 		Move.Actions = {};
 
 		function TryPickANumber(Number)
@@ -88,24 +145,22 @@ class TPickANumberGame extends TGame
 			if ( this.State.Numbers[Number] !== null )
 				throw `Number ${Number} already picked`;
 			
-			const PickingPlayer = this.Players[this.CurrentPlayerIndex];
-			this.State.Numbers[Number] = PickingPlayer;
-			this.CurrentPlayerIndex = (this.CurrentPlayerIndex+1)%this.Players.length;
+			this.State.Numbers[Number] = NextPlayer;
+			this.EndPlayerTurn(NextPlayer);	//	move to next player
 			
 			//	reply with move data send to all players
 			const ActionRender = {};
-			ActionRender.Player = PickingPlayer;
-			ActionRender.Debug = `Player ${PickingPlayer} picked ${Number}`;
+			ActionRender.Player = NextPlayer;
+			ActionRender.Debug = `Player ${NextPlayer} picked ${Number}`;
 			return ActionRender;
 		}
 		
 		function ForfeitMove(Error)
 		{
-			const PickingPlayer = this.Players[this.CurrentPlayerIndex];
-			this.CurrentPlayerIndex = (this.CurrentPlayerIndex+1)%this.Players.length;
-
+			this.EndPlayerTurn(NextPlayer);	//	move to next player
+			
 			const ActionRender = {};
-			ActionRender.Player = PickingPlayer;
+			ActionRender.Player = NextPlayer;
 			ActionRender.Debug = `Move forfeigted ${Error}`;
 			return ActionRender;
 		}
