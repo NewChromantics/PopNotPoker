2e23c3b07a437c6ba86cbc8cf189a037a1bbcdc1
Graham Reeves <graham@grahamreeves.com>
2020-06-12 23:52:08 +0100

    Reworked client side, so debug display is now a kind of game-UI

--- Client/bootup.js
+++ Client/bootup.js
@@ -1,17 +1,98 @@
 
-//	generic/debug ui
-const StateGui = new Pop.Gui.Label('CurrentState');
-const ActionGui = new Pop.Gui.Label('LastAction');
-const DebugGui = new Pop.Gui.Label('Debug');
-const ErrorGui = new Pop.Gui.Label('Error');
+class DebugGameClient
+{
+	constructor()
+	{
+		//	generic/debug ui
+		this.StateGui = new Pop.Gui.Label('CurrentState');
+		this.ActionGui = new Pop.Gui.Label('LastAction');
+		this.DebugGui = new Pop.Gui.Label('Debug');
+		this.ErrorGui = new Pop.Gui.Label('Error');
+	}
+	
+	ClearMoveActionButtons()
+	{
+		const ButtonContainer = document.querySelector('#MoveButtonContainer');
+		
+		//	delete old content
+		while (ButtonContainer.firstChild)
+			ButtonContainer.removeChild(ButtonContainer.lastChild);
+	}
+
+	Show()
+	{
+		const DebugContainer = document.querySelector('#DebugGui');
+		DebugContainer.style.display = 'block';
+	}
+	
+	UpdateState(State,Message)
+	{
+		this.StateGui.SetValue(JSON.stringify(State,null,'\t'));
+	}
+	
+	OnAction(Message)
+	{
+		this.ClearMoveActionButtons();
+		this.ActionGui.SetValue(JSON.stringify(Message,null,'\t'));
+	}
+	
+	OnMoveRequest(Move,SendReplyAction)
+	{
+		this.ClearMoveActionButtons();
+		const ButtonContainer = document.querySelector('#MoveButtonContainer');
+
+		//	add button for each action choice
+		function AddActionButton(ActionName)
+		{
+			const Action = Move.Actions[ActionName];
+			const Div = document.createElement('div');
+			ButtonContainer.appendChild(Div);
+			const Button = document.createElement('input');
+			Div.appendChild(Button);
+			Button.type = 'button';
+			Button.value = ActionName;
+			
+			function MakeArgumentInput(ArgumentChoices)
+			{
+				const Input = document.createElement('select');
+				Div.appendChild(Input);
+				function AddOption(ArgumentValue)
+				{
+					const Option = document.createElement('option');
+					Option.value = ArgumentValue;
+					Option.text = ArgumentValue;
+					Input.appendChild(Option);
+				}
+				ArgumentChoices.forEach(AddOption);
+				return Input;
+			}
+			const ArgumentInputs = Action.Arguments.map(MakeArgumentInput);
+			
+			function OnClick()
+			{
+				const ArgumentValues = ArgumentInputs.map( i => i.value );
+				SendReplyAction(ActionName,ArgumentValues);
+			}
+			Button.onclick = OnClick;
+		}
+		Object.keys(Move.Actions).forEach(AddActionButton);
+		
+		this.ErrorGui.SetValue(Move.LastMoveError||"");
+	}
+	
+	OnOtherMessage(Message,SendReply)
+	{
+		this.DebugGui.SetValue(JSON.stringify(Message,null,'\t'));
+	}
+}
 
 const Games = {};
+let DebugGame = new DebugGameClient();
 
 function AllocGame(GameType,SendCommand)
 {
 	switch(GameType)
 	{
-		case null:	//	not currently reporting on server
 		case 'TMinesweeperGame':
 			return new TMinesweeperClient(SendCommand);
 	}
@@ -35,66 +116,6 @@ function GetGame(Packet,SendCommand)
 	return Games[GameHash];
 }
 
-function ClearMoveActionButtons()
-{
-	const ButtonContainer = document.querySelector('#MoveButtonContainer');
-	
-	//	delete old content
-	while (ButtonContainer.firstChild)
-		ButtonContainer.removeChild(ButtonContainer.lastChild);
-}
-
-function OnMoveRequest(Move,SendReply)
-{
-	ClearMoveActionButtons();
-	const ButtonContainer = document.querySelector('#MoveButtonContainer');
-
-	function SendReplyAction(ActionName,Arguments)
-	{
-		const Reply = {};
-		Reply.Action = ActionName;
-		Reply.ActionArguments = Arguments;
-		SendReply(Reply);
-	}
-	
-	//	add button for each action choice
-	function AddActionButton(ActionName)
-	{
-		const Action = Move.Actions[ActionName];
-		const Div = document.createElement('div');
-		ButtonContainer.appendChild(Div);
-		const Button = document.createElement('input');
-		Div.appendChild(Button);
-		Button.type = 'button';
-		Button.value = ActionName;
-		
-		function MakeArgumentInput(ArgumentChoices)
-		{
-			const Input = document.createElement('select');
-			Div.appendChild(Input);
-			function AddOption(ArgumentValue)
-			{
-				const Option = document.createElement('option');
-				Option.value = ArgumentValue;
-				Option.text = ArgumentValue;
-				Input.appendChild(Option);
-			}
-			ArgumentChoices.forEach(AddOption);
-			return Input;
-		}
-		const ArgumentInputs = Action.Arguments.map(MakeArgumentInput);
-		
-		function OnClick()
-		{
-			const ArgumentValues = ArgumentInputs.map( i => i.value );
-			SendReplyAction(ActionName,ArgumentValues);
-		}
-		Button.onclick = OnClick;
-	}
-	Object.keys(Move.Actions).forEach(AddActionButton);
-	
-	ErrorGui.SetValue(Move.LastMoveError||"");
-}
 
 function OnMessage(Message,SendReply)
 {
@@ -106,60 +127,67 @@ function OnMessage(Message,SendReply)
 		SendReply(Packet);
 	}
 	
-	function SendReplyWithHash(Reply)
-	{
-		Reply.Hash = Message.Hash;
-		Reply.Command = Message.ReplyCommand;
-		SendReply(Reply);
-	}
-	
-	if ( Message.Error || Message.Debug )
-	{
-		ErrorGui.SetValue(JSON.stringify(Message,null,'\t'));
-	}
-	
 	if ( Message.Command == 'Ping' )
 	{
 		//	pong?
 		return;
 	}
+
 	
+	if ( Message.Error || Message.Debug )
+	{
+		Pop.Debug(`Message Error/Debug ${JSON.stringify(Message,null,'\t')}`);
+		//ErrorGui.SetValue(JSON.stringify(Message,null,'\t'));
+	}
 	//	no meta, just some error message?
 	if ( !Message.Meta )
 	{
-		DebugGui.SetValue(JSON.stringify(Message,null,'\t'));
+		Pop.Debug(`Message no meta ${JSON.stringify(Message,null,'\t')}`);
+		//DebugGui.SetValue(JSON.stringify(Message,null,'\t'));
 		return;
 	}
 	
-	const Game = GetGame(Message,SendCommand);
+	
+	//	set the debug game
+	let Game = DebugGame;
+	try
+	{
+		Game = GetGame(Message,SendCommand);
+	}
+	catch(e)
+	{
+		DebugGame.Show();
+	}
 
 	//	update state
 	if ( Message.State )
 	{
-		StateGui.SetValue(JSON.stringify(Message));
+		DebugGame.UpdateState(Message.State,Message);
 		Game.UpdateState(Message.State,Message);
 	}
 
 	//	render an action
 	if ( Message.Action )
 	{
-		ClearMoveActionButtons();
-		ActionGui.SetValue(JSON.stringify(Message,null,'\t'));
+		DebugGame.OnAction(Message);
 		Game.OnAction(Message);
 	}
 	
 	//	gr: should we ditch .command?
 	if ( Message.Command == 'Move' )
 	{
-		OnMoveRequest(Message.Move,SendReplyWithHash);
 		function SendReplyAction(ActionName,Arguments)
 		{
 			const Reply = {};
 			Reply.Action = ActionName;
 			Reply.ActionArguments = Arguments;
-			SendReplyWithHash(Reply);
+			Reply.Hash = Message.Hash;
+			Reply.Command = Message.ReplyCommand;
+			SendReply(Reply);
 		}
-		return Game.OnMoveRequest(Message.Move,SendReplyAction);
+		DebugGame.OnMoveRequest(Message.Move,SendReplyAction);
+		Game.OnMoveRequest(Message.Move,SendReplyAction);
+		return;
 	}
 	else if ( Message.Command )
 	{
@@ -167,10 +195,11 @@ function OnMessage(Message,SendReply)
 	}
 	else
 	{
+		DebugGame.OnOtherMessage(Message,SendReply);
 		Game.OnOtherMessage(Message,SendReply);
 	}
 
-	DebugGui.SetValue(JSON.stringify(Message,null,'\t'));
+	//DebugGui.SetValue(JSON.stringify(Message,null,'\t'));
 }
 
 async function ConnectToServerLoop(GetAddress,OnMessage)
@@ -234,9 +263,9 @@ function GetNextAddress()
 
 	//	let user insert hostname/port
 	if (Pop.GetExeArguments().Hostname)
-		HostNames.splice(0,0,Pop.GetExeArguments().Hostname);
+		HostNames = [Pop.GetExeArguments().Hostname];
 	if (Pop.GetExeArguments().Port)
-		Ports.splice(0,0,Pop.GetExeArguments().Port);
+		Ports = [Pop.GetExeArguments().Port];
 
 	for (let Hostname of HostNames)
 		for (let Port of Ports)
