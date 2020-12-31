58ba90cb0734e3beb61e59a658810faca367d408
Graham Reeves <graham@grahamreeves.com>
2020-12-31 12:35:07 +0000

    Lobby node server now detects port room boots on

--- Lobby.js
+++ Lobby.js
@@ -1,6 +1,7 @@
 /*
 	this is the lobby nodejs server.
 	
+
 	It has a few functions
 	* Create new room
 		- Spin up new process and associate a name and port with it
@@ -11,9 +12,32 @@
 
 const Pop = {};
 Pop.Debug = console.log;
+Pop.CreatePromise = function()
+{
+	let Callbacks = {};
+	let PromiseHandler = function(Resolve,Reject)
+	{
+		Callbacks.Resolve = Resolve;
+		Callbacks.Reject = Reject;
+	}
+	let Prom = new Promise(PromiseHandler);
+	Prom.Resolve = Callbacks.Resolve;
+	Prom.Reject = Callbacks.Reject;
+	return Prom;
+}
 
-const PopExe = '/home/app/node_modules/@newchromantics/popengine/ubuntu-latest/PopEngineTestApp';
-const PopAppPath = '/home/app';
+
+const MacPopExe = '/Volumes/Code/PopEngine/build/Debug_JavascriptCore/PopEngine.app/Contents/MacOS/PopEngine';
+const MacRoomAppPath = './Server';
+
+//	fallback to mac local test settings 
+//	if paths not provided by env vars (expect these to be set in dockerfile)
+const RoomAppPath = process.env.RoomAppPath || MacRoomAppPath;
+const PopExe = process.env.PopExe || MacPopExe;
+
+
+const { spawn } = require( "child_process" );
+
 
 //	returns meta
 async function StartRoomProcess()
@@ -23,77 +47,52 @@ async function StartRoomProcess()
 
 	//	
 	const RoomProcess = spawn( PopExe, [
-		PopAppPath,
-		`RayDataFilename=${RayDataFilename}`,
-		`ObjFilename=${SceneObjFilename}`,
-		`ZipSaveLocation=${ZipSaveLocation}`,
-		`TimeOfRun=${Date()}`,
-		`ServerVersion=${pjson.version}`,
-		`SeverDependencies=${pjson.dependencies}`,
-		`NodeVersion=${process.versions.node}`
+		RoomAppPath,
+		`Port=1`
 	] );
 	Meta.Process = RoomProcess;
-	Meta.Port = 10001;
+	Meta.Port = false;
 	
-	return Meta;
-	/*
-	log = "";
-	let ZipFile = "";
+	const ListeningPortPattern = new RegExp('Listening on ([0-9]+)\n');
+	const ListeningPortPromise = Pop.CreatePromise();
+	
 	RoomProcess.stdout.on( "data", ( data ) =>
 	{
-		console.log( `stdout: ${data}` );
-		log += data;
-		let StringData = data.toString();
-
-		if ( StringData.startsWith( "Zipname" ) )
+		//	convert to string
+		data = `${data}`;
+		const DataClean = data.replace('\n','\\n');
+		console.log( `stdout: ${DataClean}` );
+		const PortMatch = data.match(ListeningPortPattern);
+		if ( PortMatch )
 		{
-			var Regex = /\w+.zip/
-			let RegexArray = Regex.exec( StringData );
-			console.log( RegexArray[ 0 ] )
-			ZipFile = RegexArray[ 0 ];
-		}
+			Pop.Debug(JSON.stringify(PortMatch));
+			const Port = parseInt(PortMatch[1]);
+			Pop.Debug(`Detected listening port: ${Port}`);
+			ListeningPortPromise.Resolve(Port);
+		}		
 	} );
 
 	RoomProcess.stderr.on( 'data', ( data ) =>
 	{
 		console.log( `stderr: ${data}` );
-		log += data;
-
-		let StringData = data.toString();
-
-		if ( StringData.startsWith( "Zipname" ) )
-		{
-			console.log(StringData)
-			var Regex = /\w+.zip/
-			let RegexArray = Regex.exec( StringData );
-			console.log( RegexArray[ 0 ] )
-			ZipFile = RegexArray[ 0 ];
-		}
 	} );
 
 	RoomProcess.on( 'error', ( error ) =>
 	{
 		console.log( `error: ${error.message}` );
-		log += error.message;
-
-		ServerResponse(res, 'error')
+		ListeningPortPromise.Reject(error.message);
 	} );
 
 	RoomProcess.on( "close", ( code ) =>
 	{
 		console.log("Finished")
-
-		res.download( ZipSaveLocation, e =>
-			{
-				if(e)
-				{
-					console.log(e);
-					ServerResponse(res, 'error')
-				}
-			})
-
+		ListeningPortPromise.Reject('Process closed');
 	} );
-	*/
+		
+	//	wait for app to bootup and put out port number (or process fail)
+	Meta.Port = await ListeningPortPromise;
+	
+	return Meta;
 	
 }
 
@@ -121,16 +120,31 @@ function GetRoom(Name)
 
 
 var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer(options);
+var http = require('http');
 
-var server = http.createServer(function(req, res) 
+// Create a basic proxy server in one line of code...
+// This listens on port 8000 for incoming HTTP requests 
+// and proxies them to port 9000
+const ProxyOptions = 
 {
-	// You can define here your custom logic to handle the request
-	// and then proxy the request.
-	proxy.web(req, res, { target: 'http://127.0.0.1:5060' });
-});
+	target: 
+	{
+		host: 'localhost',
+		port: 1234		
+	}
+}
+const Proxy = httpProxy.createProxyServer(ProxyOptions);
+//httpProxy.createServer(ProxyOptions).listen(8000);
 
-function OnUpgrade(req, socket, head)
+
+function OnHttpRequest(Request,Result)
+{
+	//	gr: don't really need this
+	const TargetUrl = 'http://localhost:9000';
+	Proxy.web(Request, Result, { target: TargetUrl });	
+}
+
+function OnUpgradeRequest(req, socket, head)
 {
 	function Event(e)
 	{
@@ -140,7 +154,25 @@ function OnUpgrade(req, socket, head)
 	const TargetUrl = `ws://localhost:${FirstRoom.Port}`;
 	Pop.Debug(`request ${req} ${JSON.stringify(req)} TargetUrl=${TargetUrl}`);
 	const NewProxyMeta = {target: TargetUrl, changeOrigin: true, ws: true}; 
-	proxy.ws(req, socket, head, NewProxyMeta, Event );
+	Proxy.ws(req, socket, head, NewProxyMeta, Event );
 }
 
-server.on('upgrade', OnUpgrade );
+const HttpServer = http.createServer(OnHttpRequest);
+HttpServer.on('upgrade', OnUpgradeRequest );
+HttpServer.listen(8000);
+
+
+// ...and a simple http server to show us our request back.
+const static = require('node-static');
+const FileServer = new(static.Server)('./Client/');
+function OnStaticRequest(req,res)
+{
+	FileServer.serve(req, res);
+	/*
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+  */
+}
+http.createServer(OnStaticRequest).listen(9000);
+
